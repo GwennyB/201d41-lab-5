8c06bf910c6b85eb57faf673c08adcf8d73624bb
Gwendolyn Zubatch <gyzubatch@gmail.com>
2018-10-26 15:16:16 -0700

    solution is same as problem 5; passes all tests

--- app.js
+++ app.js
@@ -133,24 +133,39 @@ testMultiplyArray(testArray);
 
 /////////////////////////////////////
 /* STRETCH GOAL: Problem 6
-Write a function called multiplyAnyArray() that takes an array of numbers of any length as its argument and returns an array whose first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and concatenates a message using the arguments that were passed into the function:
+Write a function called multiplyAnyArray() that takes an array of numbers of any length as its argument and returns an array whose first element is the 
+product of those numbers, and the second element is a string that EXACTLY follows this example and concatenates a message using the arguments that were passed 
+into the function:
 
 "The numbers 1,2,3,4,5 have a product of 120."
 
-IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this.
+IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. 
+You're going to have to be resourceful to figure out how to do this.
 
 This function should be dynamic, accepting an array of any length.
 
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyAnyArray() function and see if the test passes.*/
+Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyAnyArray() function and 
+see if the test passes.*/
 
 // Write your code here
 var testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
+  var multiplied = 1;
+  var numList = '';
+  for (var i=0; i<dynamicArray.length; i++) {
+    multiplied = multiply(multiplied,dynamicArray[i])[0];
+    numList += dynamicArray[i] + ',';
+  }
+  numList = numList.substring(0,numList.length-1);
+  return [multiplied, 'The numbers ' + numList + ' have a product of ' + multiplied + '.'];
+}  
 
-}
+// Solution is same as Problem 5.
+
+
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyAnyArray(testDynamicArray);
+testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
